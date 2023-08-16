d90ea634aafac119d781a30a0f235d129ecbe066
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-17 08:41:47 +0900

    :recycle: [ch8] 02-1

--- ch8/02-1.js
+++ ch8/02-1.js
@@ -1,29 +1,32 @@
 class Customer {
   _name;
-  _discountRate;
   _contract;
 
   constructor(name, discountRate) {
     this._name = name;
-    this._discountRate = discountRate;
-    this._contract = new CustomerContract(new Date());
-  }
-  get discountRate() {
-    return this._discountRate;
+    this._contract = new CustomerContract(new Date(), discountRate);
   }
   becomePreferred() {
-    this._discountRate += 0.03;
+    this._contract._discountRate += 0.03;
     // do other stuff
   }
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._discountRate));
+    return amount.subtract(amount.multiply(this._contract._discountRate));
   }
 }
 
 class CustomerContract {
   _startDate;
-  constructor(startDate) {
+  _discountRate;
+  constructor(startDate, discountRate) {
     this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+  get discountRate() {
+    return this._discountRate;
+  }
+  set discountRate(value) {
+    this._discountRate = value;
   }
 }
 
