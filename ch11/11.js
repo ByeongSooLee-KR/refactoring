03ca37c7cfcd21cd53a435ec995996bc8bbcdfd4
ByeongSooLee <djaxornwkd12@gmail.com>
2023-09-14 18:56:53 +0900

    :recycle: Refactor: ch11.11

--- ch11/11.js
+++ ch11/11.js
@@ -1,19 +1,21 @@
 // 11.11 수정된 값 반환하기
 const calculateAscent = () => {
   for (let i = 1; i < points.length; i++) {
+    let result = 0;
     const verticalChange = points[i].elevation - points[i - 1].elevation;
-    totalAscent += verticalChange > 0 ? verticalChange : 0;
-    // 외부에 있는 변수를 업데이트 하고 있음
+    result += verticalChange > 0 ? verticalChange : 0;
   }
+  return result;
 };
 const calculateTime = () => {};
 const calculateDistance = () => {};
 
 let points = [];
-let totalAscent = 0;
+let totalAscent = calculateAscent();
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
+
+// 함수 내부에서 외부 변수를 업데이트 하거나 전달 받은 객체를 업데이트할 때는 조심해야함
