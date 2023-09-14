a972efb8ba6ca13da135a0a9a5a6e0c19603aa1a
ByeongSooLee <djaxornwkd12@gmail.com>
2023-09-14 18:45:13 +0900

    :recycle: Refactor: 11.06

--- ch11/06.js
+++ ch11/06.js
@@ -1,9 +1,11 @@
 // 11.06 질의 함수를 매개변수로 바꾸기
 
-targetTemperature(aPlan);
+targetTemperature(aPlan, thermostat.currentTemperature);
 
 // 다른모듈에 있는 함수라고 가정
-function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+function targetTemperature(aPlan, currentTemperature) {
+  currentTemperature = currentTemperature;
+  // thermostat 객체에 의존하고 있는데 외부의 모듈, 속성에 의존하는 것은 심각한 coupling 발생
+  // -> 매개변수로 받는 것이 적절
   //...
 }
