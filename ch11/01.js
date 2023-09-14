d9a6cb2317c77ab15a79f6eca5a7b909c1e5f876
ByeongSooLee <djaxornwkd12@gmail.com>
2023-09-14 18:18:27 +0900

    11-01 before

--- ch11/01.js
+++ ch11/01.js
@@ -1,6 +1,11 @@
+//11-01 질의 함수와 변경함수 분리하기
+
+//Side Effect 생기지 않게
+// 함수명과 다른일을 하지 않도록 것이 중요
 const setOffAlarms = () => {
   console.warn("악당을 찾았소");
 };
+// 두 가지 일을 하고 있음 (악당을 찾는 로직과 호출하는 로직)
 const alertForMiscreant = (people) => {
   for (const p of people) {
     if (p === "조커") {
